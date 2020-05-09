44905f2fb8d315ea4a53473ec6ba1f1cdb6f4e7a
Евгений Серебряков <eserebriakov@Evgenijs-iMac.Dlink>
2020-05-09 14:28:21 +0300

    Add eslint

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const http = require('http')
 const path = require('path')
 
-const static = require('node-static')
+const nodeStatic = require('node-static')
 
-const file = new static.Server(path.resolve(__dirname, '../dist'))
+const file = new nodeStatic.Server(path.resolve(__dirname, '../dist'))
 
 const server = http.createServer((req, res) => {
-  file.serve(req, res);
+  file.serve(req, res)
 })
 
 server.listen(8081, () => {
